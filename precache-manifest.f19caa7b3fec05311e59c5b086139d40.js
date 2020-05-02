7d38fb0aa5c1536ba6ef0806bcab459ac050dcf3
Damian Trzeciak <dtm-github.com@trzeciak.biz>
2020-05-02 20:40:51 +0200

    Updates

---
+++ precache-manifest.f19caa7b3fec05311e59c5b086139d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c37e5ef91a7f9b3ef52a41c1faa348",
+    "url": "/RoyalGameOfUr/index.html"
+  },
+  {
+    "revision": "d585f9a8d4217bf41434",
+    "url": "/RoyalGameOfUr/static/css/main.43b69f8e.chunk.css"
+  },
+  {
+    "revision": "c1135c3648959c335e3f",
+    "url": "/RoyalGameOfUr/static/js/2.69486de2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoyalGameOfUr/static/js/2.69486de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d585f9a8d4217bf41434",
+    "url": "/RoyalGameOfUr/static/js/main.e8f515a1.chunk.js"
+  },
+  {
+    "revision": "c0dd99235c78a11cfd2d",
+    "url": "/RoyalGameOfUr/static/js/runtime-main.808d31be.js"
+  }
+]);
