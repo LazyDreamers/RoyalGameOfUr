be88148ad7a705a30525d20a56e1e77edc028e23
bot <bot>
2020-05-10 15:47:27 +0000

    Updates

--- precache-manifest.995f64755ec96b698527a9cee86c1390.js
+++ precache-manifest.995f64755ec96b698527a9cee86c1390.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62b02294a8bdb141934de28a8f734b5f",
+    "revision": "6a71dafda6e70e05ada4224957ae90ce",
     "url": "/RoyalGameOfUr/index.html"
   },
   {
-    "revision": "61636d9c5bb0bd70c946",
+    "revision": "52a5c1d4d17341368f68",
     "url": "/RoyalGameOfUr/static/css/main.029321ed.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/RoyalGameOfUr/static/js/2.f43c8577.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "61636d9c5bb0bd70c946",
-    "url": "/RoyalGameOfUr/static/js/main.29a810d4.chunk.js"
+    "revision": "52a5c1d4d17341368f68",
+    "url": "/RoyalGameOfUr/static/js/main.fba26bf2.chunk.js"
   },
   {
     "revision": "c0dd99235c78a11cfd2d",
